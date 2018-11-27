fd83b09eeeba0588e4d079ffe28d82a9f2124b61
Patrick Hulce <patrick.hulce@gmail.com>
2018-11-27 15:59:32 -0600

    feat: add PSI script and cron

---
+++ ansible/files/scripts/agent-run-psi.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export LH_PATH="/dzl/src/lighthouse"
+export DZL_PATH="/dzl/src/dzl/cli"
+export DZL_CONFIG_FILE="/dzl/conf/agent-psi.config.js"
+
+if [ -e /dzl/log/dzl-off ]; then
+  echo "DZL is off, remove /dzl/log/dzl-off to turn back on."
+  exit 1
+fi
+
+if [ -e /dzl/conf/tokens.sh ]; then
+  source /dzl/conf/tokens.sh
+  echo "PSI Token is $(echo $PSI_TOKEN | head -c 4)..."
+else
+  echo "No PSI Token is set, cannot continue."
+  exit 1
+fi
+
+cd "$LH_PATH" || exit 1
+git checkout -f origin/master || exit 1
+git pull origin master || exit 1
+
+cd "$DZL_PATH" || exit 1
+git checkout -f master || exit 1
+git pull origin master || exit 1
+yarn install || exit 1
+
+node ./bin/dzl.js collect --limit=1 --label="continuous-psi" --concurrency=1 --child-process-concurrency=15 --config=$DZL_CONFIG_FILE
+DZL_EXIT_CODE=$?
+
+if [ $DZL_EXIT_CODE -eq 0 ]; then
+  echo "Success!"
+else
+  echo "Failed, exiting with error code 1"
+  exit 1
+fi
+
+
---
+++ ansible/templates/conf/agent-psi.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  collector: {
+    type: 'psi',
+    apiKey: process.env.PSI_TOKEN,
+    repositoryPath: process.env.LH_PATH,
+  },
+  collection: {
+    runs: 25,
+    urls: [
+      'http://localhost:10200/online-only.html',
+      'http://localhost:10200/perf/fonts.html',
+      'http://localhost:10200/byte-efficiency/tester.html',
+      'http://localhost:10200/a11y/a11y_tester.html',
+      'https://example.com',
+      'https://www.sfgate.com',
+      'https://www.theverge.com',
+      'https://www.cnn.com',
+      'https://www.cnet.com',
+      'https://www.amazon.com',
+      'https://www.att.com',
+      'https://www.hulu.com',
+      'https://www.linkedin.com',
+      'https://www.facebook.com',
+      'https://www.vevo.com',
+      'https://www.wikipedia.org',
+    ],
+  },
+  storage: {
+    type: 'sql',
+    host: '{{ hostvars[groups["masters"][0]].ansible_default_ipv4.address }}',
+  },
+  lighthouseConfig: {
+    extends: 'lighthouse:default',
+    settings: {onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo', 'pwa']},
+  },
+}
